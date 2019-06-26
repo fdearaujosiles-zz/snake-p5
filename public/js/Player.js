2be5b090cb3d25719d096ab65aeb834f121c4fb4
Siles <fernando.siles@nokia.com>
2019-06-26 15:56:28 -0300

    Game playable

--- public/js/Player.js
+++ public/js/Player.js
@@ -3,43 +3,63 @@ class Player {
       let centerX = width / 2;
       let centerY = height / 2;
       this.sqrSize = 10;
-      this.squares = [[centerX, centerY], 
-                      [centerX-this.sqrSize, centerY], 
-                      [centerX-(this.sqrSize * 2), centerY], 
-                      [centerX-(this.sqrSize * 3), centerY], 
-                      [centerX-(this.sqrSize * 4), centerY]];
+      this.body = [createVector(centerX, centerY), 
+                      createVector(centerX-this.sqrSize, centerY), 
+                      createVector(centerX-(this.sqrSize * 2), centerY), 
+                      createVector(centerX-(this.sqrSize * 3), centerY), 
+                      createVector(centerX-(this.sqrSize * 4), centerY)];
       this.dir = [1, 0];
       this.nextMove = undefined;
       this.nextDir = [1, 0];
     }
   
-    get length() {return this.squares.length}
+    get x() {return this.body[0].x}
+    get y() {return this.body[0].y}
+    get length() {return this.body.length}
+
+    segment(n) {return [this.body[n].x, this.body[n].y, this.sqrSize, this.sqrSize]}
   
     update(xOffset, yOffset) {
-      console.log(xOffset, yOffset)
-      this.nextMove = this.squares.map((s,i) => {
-        if(i!=0) return this.squares[i-1]
+      this.nextMove = this.body.map((s,i) => {
+        if(i!=0) return this.body[i-1]
         else {
-          let newPosX = s[0] + (this.sqrSize * xOffset), newPosY = s[1] + (this.sqrSize * yOffset);
-          return [newPosX>=width ? newPosX-width : newPosX<0 ? newPosX+width : newPosX,
-                  newPosY>=height ? newPosY-height : newPosY<0 ? newPosY+height : newPosY]
+          let newPosX = s.x + (this.sqrSize * xOffset), newPosY = s.y + (this.sqrSize * yOffset);
+          return createVector(newPosX>=width ? newPosX-width : newPosX<0 ? newPosX+width : newPosX,
+                  newPosY>=height ? newPosY-height : newPosY<0 ? newPosY+height : newPosY)
         }
       })
       this.nextDir = [xOffset, yOffset]
     }
   
+    hasEaten(food) {
+      if(dist(this.x, this.y, food.x, food.y) < 1) {
+        this.grow();
+        return true
+      }
+      return false
+    }
+
+    collide() {
+      for(let i=1; i<this.length;i++) {
+        if(dist(this.body[0].x ,this.body[0].y, this.body[i].x, this.body[i].y) < 1) {
+          return true
+        }
+      }
+      return false
+    }
+
     show() {
       background(30);
       fill(255);
-      this.squares = this.nextMove;
+      this.body = this.nextMove;
       this.dir = this.nextDir;
       this.nextMove = undefined;
       this.nextDir = undefined;
       fill(255, 40, 40, 255);
-      rect(...this.squares[0].concat([this.sqrSize, this.sqrSize]));
+      rect(...this.segment(0));
       fill(120, 255, 120, 255);
-      for(let i=1; i<this.length;i++) rect(...this.squares[i].concat([this.sqrSize, this.sqrSize]));
+      for(let i=1; i<this.length;i++) rect(...this.segment(i));
     }
   
-    grow() {this.squares = [...this.squares, []]}
+    grow() {this.body = [...this.body, []]}
 }
--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,31 +1,53 @@
-var player, frameSkip = 8;
+var player, food, gameStart = false, speed = 7;
+
+function createFood() {
+  return createVector(floor(random((width/10) - 10)) * 10, floor(random((height/10) - 10)) * 10)
+}
 
 function setup() {
   createCanvas(400, 400);
+  frameRate(speed)
+  background(30)
   player = new Player();
-  food = new Food();
+  player.update(...player.dir);
+  player.show();
+  food = createFood();
+  rect(food.x, food.y, 10, 10)
 }
 
 function draw() {
-  if(frameCount % frameSkip == 0) {
+  if(gameStart){
     if(!player.nextMove) player.update(...player.dir);
     player.show();
+    if(player.hasEaten(food)) food = createFood();
+    if(player.collide()) {
+      gameStart = false;
+      player = new Player();
+      player.update(...player.dir);
+      player.show();
+      food = createFood();
+    }
+    rect(food.x, food.y, 10, 10)
   }
 }
 
 function keyPressed() {
-  switch(keyCode) {
-    case LEFT_ARROW:
-      if(player.dir[0] == 0) player.update(-1,0);
-      break;
-    case RIGHT_ARROW:
-      if(player.dir[0] == 0) player.update(1,0);
-      break;
-    case DOWN_ARROW:
-      if(player.dir[1] == 0) player.update(0,1);
-      break;
-    case UP_ARROW:
-      if(player.dir[1] == 0) player.update(0,-1);
-      break;
+  if(gameStart){
+    switch(keyCode) {
+      case LEFT_ARROW:
+        if(player.dir[0] == 0) player.update(-1,0);
+        break;
+      case RIGHT_ARROW:
+        if(player.dir[0] == 0) player.update(1,0);
+        break;
+      case DOWN_ARROW:
+        if(player.dir[1] == 0) player.update(0,1);
+        break;
+      case UP_ARROW:
+        if(player.dir[1] == 0) player.update(0,-1);
+        break;      
+    }
+  } else if (keyCode == RETURN) {
+    gameStart = true;
   }
 }
